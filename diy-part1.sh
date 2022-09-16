73104095b93aa389a2180221c8c62bc72cc0fd7d
sertt <sertt168@163.com>
2022-09-16 22:37:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,7 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/kenzok8/openwrt-packages
 
 
 
